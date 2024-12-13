dbe30134f85c441a38fa73c39ea714c003eacfe1
ShreyaP707 <shreyaparmar707@gmail.com>
2024-12-13 14:17:30 +0530

    Create user.java

---
+++ investor_user/user.java
@@ -0,0 +1,10 @@
+package com.platform.model;
+
+public class User {
+    private String username;
+    private String password;
+    private String email;
+    private String profileDetails;
+
+    // Getters and Setters
+}
