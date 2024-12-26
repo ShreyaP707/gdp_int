d2c8a56cf9d6426ecd046fac37f10dde2ca9cb88
ShreyaP707 <shreyaparmar707@gmail.com>
2024-12-26 14:19:47 +0530

    Update .java

--- servlet/userservlet/.java
+++ servlet/userservlet/.java
@@ -10,3 +10,10 @@ protected void doPost(HttpServletRequest request, HttpServletResponse response) 
     // Redirect to the profile page
     response.sendRedirect("profile.jsp");
 }
+@Override
+protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    // Assuming user data is set in request
+    User user = userDAO.getUser ByUsername("someUsername"); // Fetch user data
+    request.setAttribute("user", user); // Set user data in request scope
+    request.getRequestDispatcher("profile.jsp").forward(request, response);
+}
